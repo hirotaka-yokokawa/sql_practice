dec39587d94fd05f7d4878c7e31870042b5b63fd
Hirotaka <yokokawa.491992@gmail.com>
2018-11-25 15:23:24 +0900

    sqlの中にwhereを入れることで条件分岐ができる｡ ex) sql = "select * from users where age < 20"20歳以下だけを選択して出力する｡

--- sample03.py
+++ sample03.py
@@ -7,6 +7,10 @@ Tomさんは27歳です
 Kenさんは77歳です
 
 って出力する
+
+
+年が20歳以下
+
 """
 import sqlite3
 
@@ -15,23 +19,23 @@ def main():
     conn = sqlite3.connect("users.sqlite")
     cursor = conn.cursor()
 
-    sql = "select * from users"
+    sql = "select * from users where age < 20"
 
     result = cursor.execute(sql)
 
     users = result.fetchall()
 
     for user in users:
+
         name = user[0]
         age = user[1]
 
+        # if age < 20:
         print(f"{name}さんは{age}歳です")
 
     conn.commit()
 
     conn.close()
-
-
 
 
 if __name__ == "__main__":
